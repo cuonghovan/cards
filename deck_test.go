56db33f980564c3851dcf29b92aea7ed195f2747
Bepatient <bepatient@framgia0208-pc.framgia.com>
2018-07-27 13:51:30 +0700

    Testing file IO

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -18,4 +19,17 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected last card of Four of Clubs but got %v", d[len(d)-1])
 	}
+}
+
+func TestWriteDeckToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_deck_test")
+	cards := newDeck()
+	cards.writeToFile("_deck_test")
+	loadedDeck := newDeckFromFile("_deck_test")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_deck_test")
 }
